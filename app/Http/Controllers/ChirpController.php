e9d39e3a8364a2749c6d94aff37f18dcb16bb3fc
zll600 <3400692417@qq.com>
2024-07-03 16:40:03 +0800

    feat: delete chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -84,6 +84,10 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
-        //
+        Gate::authorize('delete', $chirp);
+        
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
     }
 }
